56034d99417317ce1933856358be5690f38c9cb5
Hardik Gunjal <hardikgunjal2928@gmail.com>
2023-07-30 00:22:52 -0400

    initial commit

---
+++ src/test/java/com/blind75/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blind75.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
